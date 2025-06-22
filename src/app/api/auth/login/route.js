89e6cef3e4d4b13ca75d23c5f6e4aae531a924d2
atif28431 <contact.atifansari@gmail.com>
2025-06-23 02:34:38 +0530

    Update route.js

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -4,7 +4,8 @@ export async function GET() {
   const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
   const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
   const redirectUri = process.env.NEXTAUTH_URL;
-  );
+
   const url = `https://${AUTH0_DOMAIN}/authorize?response_type=code&client_id=${AUTH0_CLIENT_ID}&redirect_uri=${redirectUri}&scope=openid%20profile%20email`;
+
   return NextResponse.redirect(url);
 }
