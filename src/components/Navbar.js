bd4ce002cd7e0c69e10880ce09109e111460c7df
Atif Ansari <muhammadatif93@gmail.com>
2025-06-24 02:11:13 +0530

    dobara

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,20 +13,15 @@ export default function Navbar() {
     const redirectUri = `${window.location.origin}/facebook-callback`;
     const scope =
       "pages_show_list,pages_manage_posts,pages_read_engagement,pages_read_user_content,public_profile,email";
-    window.location.href = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=${appId}&redirect_uri=${encodeURIComponent(
+    window.location.href = `https://www.facebook.com/v19.0/dialog/oauth?client_id=${appId}&redirect_uri=${encodeURIComponent(
       redirectUri
     )}&scope=${scope}&response_type=token&auth_type=rerequest`;
   };
 
   const connectInstagram = () => {
-    // IMPORTANT: Make sure this is the Instagram App ID, not the Facebook App ID
-    // You can find this in the Facebook Developer Console under
-    // Instagram Basic Display > Basic Display > Instagram App ID
     const appId = process.env.NEXT_PUBLIC_INSTAGRAM_APP_ID;
     const redirectUri = `${window.location.origin}/instagram-callback`;
-    const scope = "instagram_basic,instagram_content_publish,pages_show_list,pages_read_engagement";
-    
-    // Log the Instagram App ID for debugging
+    const scope = "instagram_business_basic,instagram_business_manage_messages,instagram_business_manage_comments,instagram_business_content_publish,instagram_business_manage_insights";
     console.log("Using Instagram App ID:", appId);
     console.log("Redirect URI:", redirectUri);
     
@@ -37,27 +32,15 @@ export default function Navbar() {
     
     console.log("Instagram authorization URL:", authUrl);
     
-    // Display alert with debugging info
-    alert(
-      "Debug Info:\n" +
-      "Instagram App ID: " + appId + "\n" +
-      "Redirect URI: " + redirectUri + "\n\n" +
-      "Make sure:\n" +
-      "1. You're using the Instagram App ID (not Facebook App ID)\n" +
-      "2. The redirect URI is configured in the app settings\n" +
-      "3. You're logged in with a test user account\n" +
-      "4. The test user has been properly added to the app"
-    );
-    
     // Redirect to the Instagram authorization page
     // Add state parameter generation
     const state = crypto.randomUUID();
     localStorage.setItem('instagram_state', state);
     
-    window.location.href = `https://www.instagram.com/oauth/authorize?force_reauth=true${new URLSearchParams({
+    window.location.href = `https://www.instagram.com/oauth/authorize?force_reauth=true&${new URLSearchParams({
       client_id: appId,
       redirect_uri: encodeURIComponent(redirectUri),
-      scope: 'instagram_basic,instagram_content_publish,pages_show_list,pages_read_engagement',
+      scope: 'instagram_business_basic,instagram_business_manage_messages,instagram_business_manage_comments,instagram_business_content_publish,instagram_business_manage_insights',
       response_type: 'code',
       state: state // Add state parameter
     })}`;
